639fc0d25dc00d9ba2d67bcc35033532d8f2ba57
electrons1014 <24guppra@gmail.com>
2026-08-19 13:22:24 -0700

    iteration implemented

--- cli-scripts/model-performance.m
+++ cli-scripts/model-performance.m
@@ -1,40 +1,54 @@
-%% set path information
 addpath(genpath(fullfile(pwd,'..')));
 RUN_PATH = input('path to simulation run directory: ', 's');
-PARAMS_PATH = fullfile(run_directory, 'parameters.json');
+COMPONENTS_PATH = fullfile(run_directory, 'components-config.json');
+METRICS_PATH = fullfile(run_directory, 'metrics-config.json');
 
-%% parse parameters from parameters.json
 component_order = ["load", "latch", "spring", "load_motor", "latch_motor"];
-component_config = jsondecode(fileread(PARAMS_PATH));
+component_config = jsondecode(fileread(COMPONENTS_PATH));
+metrics = jsondecode(fileread(METRICS_PATH));
 
-% validate parameters.json (incomplete!)
 if length(component_config) ~= length(component_order)
     error('parameters.json must describe all 5 components');
+end
 
-if !isequal({component_order{:}}, {component_config.component})
+if ~isequal({component_order{:}}, {component_config.component})
     error('parameters.json must order components as load, latch, spring, load motor, latch motor');
+end
 
-% identify iterable parameters
 
 iterable_params = findIterable(component_config);
+num_iterable = size(iterable_params, 1);
+num_runs = 1;
+for param = 1:num_iterable
+    num_runs = num_runs .* size(iterable_params{param, 4}, 2);
+end
 
+component_names = {component_config.name};
 
+for run = 1:num_runs
+    run_params = setRunParams(component_config, iterable_params);
 
+    componenets = cell(1,5);
+    for component = 1:length(component_config)
+        component_params = struct2cell(run_params{component});
+        components{component} = feval(component_names{component}, component_params{:});
+    end
 
-components = cell(1,5);
+    [lamsa_metrics, mda_metrics] = simulate(components, metrics);
 
-
-% initialize components
-load = feval(params(1).name, struct2cell(params(1).parameters){:});
-latch = feval(params(2).name, struct2cell(params(2).parameters){:});
-spring = feval(params(3).name, struct2cell(params(3).parameters){:});
-load_motor = feval(params(4).name, struct2cell(params(4).parameters){:});
-latch_motor = feval(params(5).name, struct2cell(params(5).parameters){:});
+    for param = 1:num_iterable
+        if iterable_params{param, 3} < size(iterable_params{param, 4}, 2)
+            iterable_params{param, 3} = iterable_params{param, 3} + 1;
+            break;
+        end
+        iterable_params{param, 3} = 1;
+    end
+end
 
 
 %% helper function to detect iterable parameters
 function iterable = findIterable(config)
-    iteratable = cell(0,0);
+    iterable = cell(0,0);
     params = {config.parameters};
 
     for component = 1:5
@@ -42,8 +56,8 @@ function iterable = findIterable(config)
         for i = 1:length(component_params)
             val = getfield(params{component}, component_params{i});
             if length(val) == 3
-                iterable = [iterable; {component, component_params{i}, 0, linspace(val(1), val(2), val(3))}];
-            elseif length(val) != 1
+                iterable = [iterable; {component, component_params{i}, 1, linspace(val(1), val(2), val(3))}];
+            elseif length(val) ~= 1
                 error('inputted parameter has wrong length. must be scalar or length 3 array');
             end
         end
@@ -51,8 +65,18 @@ function iterable = findIterable(config)
 end
 
 
+%% helper function to set run parameters
+function run = setRunParams(config, iterable)
+    run = {config.parameters};
+    num_iterable = size(iterable, 1);
+    for param = 1:num_iterable
+        run{iterable{param, 1}} = setfield(run{iterable{param, 1}}, iterable{param, 2}, iterable{param, 4}(1));
+    end
+end
+
+
 %% helper function to run LaMSA and MDA simulations + report values for given components
-function [lamsa_metrics, mda_metrics] = simulate(components)
+function [lamsa_met, mda_met] = simulate(components, mets)
 
     load = components{1};
     latch = components{2};
@@ -60,23 +84,8 @@ function [lamsa_metrics, mda_metrics] = simulate(components)
     load_motor = componenets{4};
     latch_motor = componenets{5};
 
-    metrics = {
-        'y_latch',
-        't_unlatch',
-        'theta_unlatch',
-        'omega_unlatch',
-        'v_unlatch',
-        'KE_unlatch',
-        't_takeoff',
-        'theta_takeoff',
-        'omega_takeoff',
-        'v_takeoff',
-        'KE_takeoff',
-        't_close'
-        };
-
     [lamsa_sol, lamsa_transitions] = solve_lamsa(load_motor, latch_motor, load, latch, spring);
-    lamsa_metrics = get_metrics(lamsa_sol, lamsa_transitions, load, metrics);
+    lamsa_met = get_metrics(lamsa_sol, lamsa_transitions, load, mets);
 
     [mda_sol, mda_transitions] = solve_direct_actuation(load_motor, load);
-    mda_metrics = get_metrics(mda_sol, mda_transitions, load, metrics);
+    mda_met = get_metrics(mda_sol, mda_transitions, load, mets);

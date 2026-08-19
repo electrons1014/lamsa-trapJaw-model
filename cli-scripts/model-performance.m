22e84e0b3b0ea1d35151a0319eb1ace2f8e93c62
electrons1014 <24guppra@gmail.com>
2026-08-19 15:59:26 -0700

    completed model-performance.m

--- cli-scripts/model-performance.m
+++ cli-scripts/model-performance.m
@@ -1,11 +1,10 @@
 addpath(genpath(fullfile(pwd,'..')));
 RUN_PATH = input('path to simulation run directory: ', 's');
-COMPONENTS_PATH = fullfile(run_directory, 'components-config.json');
-METRICS_PATH = fullfile(run_directory, 'metrics-config.json');
+COMPONENTS_PATH = fullfile(RUN_PATH, 'components-config.json');
+METRICS_PATH = fullfile(RUN_PATH, 'metrics-config.json');
 
 component_order = ["load", "latch", "spring", "load_motor", "latch_motor"];
 component_config = jsondecode(fileread(COMPONENTS_PATH));
-metrics = jsondecode(fileread(METRICS_PATH));
 
 if length(component_config) ~= length(component_order)
     error('parameters.json must describe all 5 components');
@@ -23,18 +22,33 @@ for param = 1:num_iterable
     num_runs = num_runs .* size(iterable_params{param, 4}, 2);
 end
 
-component_names = {component_config.name};
+
+metrics = sort(jsondecode(fileread(METRICS_PATH)));
+num_metrics = length(metrics);
+
+sim_output = cell(1, num_iterable + 2*length(metrics));
+for param = 1:num_iterable
+    sim_output{1,param} = iterable_params{param, 2};
+end
+for metric = 1:num_metrics
+    sim_output{1, num_iterable + metric} = strcat('lamsa-', metrics{metric});
+    sim_output{1, num_iterable + num_metrics + metric} = strcat('mda-', metrics{metric});
+end
+
 
 for run = 1:num_runs
     run_params = setRunParams(component_config, iterable_params);
-
-    componenets = cell(1,5);
-    for component = 1:length(component_config)
-        component_params = struct2cell(run_params{component});
-        components{component} = feval(component_names{component}, component_params{:});
-    end
+    components = initializeComponenets(component_config, run_params);
 
     [lamsa_metrics, mda_metrics] = simulate(components, metrics);
+    run_output = cell(1, num_iterable);
+    for param = 1:num_iterable
+        run_output{1,param} = getfield(run_params{iterable_params{param, 1}}, iterable_params{param, 2});
+    end
+    run_output = [run_output, values(lamsa_metrics)];
+    run_output = [run_output, values(mda_metrics)];
+
+    sim_output = [sim_output; run_output]
 
     for param = 1:num_iterable
         if iterable_params{param, 3} < size(iterable_params{param, 4}, 2)
@@ -45,47 +59,4 @@ for run = 1:num_runs
     end
 end
 
-
-%% helper function to detect iterable parameters
-function iterable = findIterable(config)
-    iterable = cell(0,0);
-    params = {config.parameters};
-
-    for component = 1:5
-        component_params = fields(params{component});
-        for i = 1:length(component_params)
-            val = getfield(params{component}, component_params{i});
-            if length(val) == 3
-                iterable = [iterable; {component, component_params{i}, 1, linspace(val(1), val(2), val(3))}];
-            elseif length(val) ~= 1
-                error('inputted parameter has wrong length. must be scalar or length 3 array');
-            end
-        end
-    end
-end
-
-
-%% helper function to set run parameters
-function run = setRunParams(config, iterable)
-    run = {config.parameters};
-    num_iterable = size(iterable, 1);
-    for param = 1:num_iterable
-        run{iterable{param, 1}} = setfield(run{iterable{param, 1}}, iterable{param, 2}, iterable{param, 4}(1));
-    end
-end
-
-
-%% helper function to run LaMSA and MDA simulations + report values for given components
-function [lamsa_met, mda_met] = simulate(components, mets)
-
-    load = components{1};
-    latch = components{2};
-    spring = components{3};
-    load_motor = componenets{4};
-    latch_motor = componenets{5};
-
-    [lamsa_sol, lamsa_transitions] = solve_lamsa(load_motor, latch_motor, load, latch, spring);
-    lamsa_met = get_metrics(lamsa_sol, lamsa_transitions, load, mets);
-
-    [mda_sol, mda_transitions] = solve_direct_actuation(load_motor, load);
-    mda_met = get_metrics(mda_sol, mda_transitions, load, mets);
+writetable(cell2table(sim_output), fullfile(RUN_PATH, 'simulation_outputs.csv'))

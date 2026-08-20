a3a66f2c4a36ac782c9573605f95fe06ad1f4b35
electrons1014 <24guppra@gmail.com>
2026-08-20 15:15:48 -0700

    fixed parsing error

--- cli-scripts/setRunParams.m
+++ cli-scripts/setRunParams.m
@@ -3,6 +3,6 @@ function run = setRunParams(config, iterable)
     run = {config.parameters};
     num_iterable = size(iterable, 1);
     for param = 1:num_iterable
-        run{iterable{param, 1}} = setfield(run{iterable{param, 1}}, iterable{param, 2}, iterable{param, 4}(1));
+        run{iterable{param, 1}} = setfield(run{iterable{param, 1}}, iterable{param, 2}, iterable{param, 4}(iterable{param, 3}));
     end
 end

755985d31b1f423e922a7e9693b32fcaceccf2af
electrons1014 <24guppra@gmail.com>
2026-08-20 03:10:23 -0700

    remove old version duplicates

--- components-library/motors/Subclasses/HillMuscleMotor.m
+++ components-library/motors/Subclasses/HillMuscleMotor.m
@@ -1,13 +1,16 @@
 %% HillMuscleMotor class definition
 % arguments in required order:
 %     muscle_length - rest length of the muscle
-%     F_motor_max - maximum amount of force the spring can exert
-%     v_motor_max - maximum velocity at which the motor can travel
+%     muscle_area - cross-sectional area of the muscle
+%     pennation_angle - angle at which muscle fibers attach to apodeme/tendon
+%     muscle_vmax - maximum velocity at which the muscle can travel (in lengths/sec)
+%     fv_curvature - curvature constant for force-velocity curve
+%     min_length - smallest length at which muscle can produce force (normalized by rest length)
+%     fl_curvature - curvature constant for force-length curve
+%     specific_tension - maximum isometric stress of the muscle
 %     r_activation - rate of activation of the muscle
-%     L_initial - initial stretch of the muscle (optional)
-%     a_L, b_L, s - phenomenological parameters fitted to describe the
-%     shape of the muscle�s length�tension curve (optional)
-% min # arguments = 4
+%     L_initial - initial stretch of the muscle (normalized by rest length)
+% min # arguments = 3
 
 classdef HillMuscleMotor < Motor
     
@@ -16,56 +19,82 @@ classdef HillMuscleMotor < Motor
         % second row contains default values for the loading motor
         % third row contains default values for the unlatching motor
         function parameters = parameters()
-            parameters = ["muscle length" "Fmax" "Vmax" "rate of activation"...
-                          "initial length" "a_L" "b_L" "s";
-                "1" "2.09E-5" "0.0003802" "0.0046" "1" "2.08" "-2.89" "-0.75";
-                "4" "10" "10" "2" "4" "2.08" "-2.89" "-0.75";
-                "0" "0" "0" "0" "0" "-Inf" "-Inf" "-Inf";
-                "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf"];
+            parameters = ["muscle_length" "muscle_area" "pennation_angle" "muscle_vmax" "fv_curvature" "min_length" "fl_curvature" "specific_tension" "r_activation" "l_initial";
+                "0" "0" "0" "5" "2.8" "0.5" "-8" "100" "200" "1";
+                "0" "0" "0" "5" "2.8" "0.5" "-8" "100" "200" "1";
+                "-Inf" "-Inf" "-Inf" "-Inf" "-Inf" "-Inf" "-Inf" "-Inf" "-Inf" "-Inf";
+                "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf" "Inf"];
         end
     end
     
     methods
-        function obj = HillMuscleMotor(muscle_length,F_motor_max,v_motor_max,r_activation,varargin)
-            % optional parameters
-            varargin_param_names = {'L_initial','a_L','b_L','s'};
-            varargin_default_values = {muscle_length,2.08,-2.89,-0.75};
+        function obj = HillMuscleMotor(muscle_length, muscle_area, pennation_angle, muscle_vmax, fv_curvature, min_length, fl_curvature, specific_tension, r_activation, L_initial)
+            
+            % parameter range verification
+            if (muscle_vmax <= 0)
+                v_motor_max = 5;
+                warning("muscle_vmax must be strictly positive, setting to 5 lengths/sec")
+            end
+            if (fv_curvature < 0)
+                fv_curvature = 2.8;
+                warning("fv_curvature must be positive, setting to 2.8")
+            end
+            if (min_length < 0) | (min_length > 1)
+                min_length = 0.5;
+                warning("min_length must be between 0 and 1 lengths, setting to 0.5 lengths")
+            end
+            if (fl_curvature < (min_length-1)^-3) | (fl_curvature > 0)
+                fl_curvature = (min_length-1)^-3;
+                warning("fl_curvature must be between (min_length - 1)^-3 and 0, setting to minimum")
+            end
+            if (specific_tension <= 0)
+                specific_tension = 100;
+                warning("specific_tension must be strictly positive, setting to 100 kPa")
+            end
+            if (r_activation <= 0)
+                r_activation = 200;
+                warning("r_activation must be strictly positive, setting to 200 Hz")
+            end
+            if (L_initial < min_length) | (L_initial > 1)
+                L_initial = 1;
+                warning("min_length must be between min_length and 1 lengths, setting to 1 length")
+            end
 
-            % check and assign optional parameters
-            if (nargin < 4)
-                error('Hill muscle motor requires at least 4 arguments.');
-            end
-            if (length(varargin)>length(varargin_param_names))
-                error('Too many input parameters');
-            end
-            for i=1:length(varargin)
-                eval([varargin_param_names{i} '=varargin{i};'])
-            end
-            for i=(length(varargin)+1):length(varargin_param_names)
-                eval([varargin_param_names{i} '=varargin_default_values{i};'])
-            end
-            if (v_motor_max == 0)
-                v_motor_max = Inf;
-                warning("v_max argument must be nonzero, setting to Inf")
-            end
-            % model
-            % motor.Force = F_motor_max * F_length * F_velocity * F_activation such that:
-            % F_length=@(t,x) (x(1)<=(L_initial-(0.7*muscle_length)))*(x(1)>=(L_initial-(1.3*muscle_length)))* exp(-((abs(((((L_initial-x(1))/muscle_length)^b_L)-1)/s))^a_L));
-            % F_velocity=@(t,x)(1-(x(2)/v_motor_max))/(1+(x(2)/(v_motor_max/4))); 
-            % F_activation=@(t,x)min(r_activation*t,1);
-            % ^ obtained from Rosario et al.
-            Force = @(t,x) (x(1)<=(L_initial-(0.7*muscle_length))) * (x(1)>=(L_initial-(1.3*muscle_length))) * F_motor_max * exp(-((abs(((((L_initial-x(1))/muscle_length)^b_L)-1)/s))^a_L)) * ((1-(abs(x(2))/v_motor_max))/(1+(abs(x(2))/(v_motor_max/4)))) .* (min(r_activation*t,1));
-            max_force = F_motor_max;
-            range=L_initial-muscle_length+(.3*muscle_length);
-        %     motor.range=muscle_length;
-            velocity=v_motor_max;
+            %%% model
+            % see here for equations: https://www.desmos.com/3d/4rgk6al4k1
+            % where alpha is in degrees, t is in seconds, l is in relative lengths, and v is in relative lengths/sec
+            % see here for full literature referenced:
+
+            % repeated values
+            fl_frac = 1/(min_length-1)^2;
+
+            Force = @(t,x) (L_initial - x(1) / muscle_length <= 1) ... % no over extension
+                        * (L_initial - x(1) / muscle_length >= min_length) ... % zero negative range of fl curve
+                        * (x(2) / muscle_length >= 0) ... % no modeling of negative velocity
+                        * (x(2) / muscle_length <= muscle_vmax) ... % zero negative range of fv curve
+                        * muscle_area * specific_tension ... % maximum isometric force
+                        * cosd(atand(sind(pennation_angle) / (cosd(pennation_angle)-1+L_initial-x(1)/muscle_length))) ... % angular correction
+                        * min(r_activation*t, 1) ... % activation curve F_time(t)
+                        * ( ... 
+                            (min_length*fl_curvature + 1 - fl_frac) ... 
+                            + (-(2*min_length+1)*fl_curvature + 2*fl_frac) * (L_initial - x(1) / muscle_length) ... 
+                            + ((min_length+2)*fl_curvature - fl_frac) * (L_initial - x(1) / muscle_length)^2 ... 
+                            + (-fl_curvature) * (L_initial - x(1) / muscle_length)^3 ... 
+                            ) ... % third order polynomial fl curve
+                        * (1 - (x(2)/muscle_length)/(muscle_vmax*cosd(atand(sind(pennation_angle) / (cosd(pennation_angle)-1+L_initial-x(1)/muscle_length))))) ... 
+                        / (1 + fv_curvature*(x(2)/muscle_length)/(muscle_vmax*cosd(atand(sind(pennation_angle) / (cosd(pennation_angle)-1+L_initial-x(1)/muscle_length))))); % fv curve
+
+            max_force = muscle_area * specific_tension * cosd(pennation_angle);
+            range= (L_initial - min_length) * muscle_length;
+            velocity = muscle_vmax * muscle_length * cosd(pennation_angle);
             
             % call parent constructor
             obj = obj@Motor(max_force, range, velocity, Force, muscle_length);
         end  
     end
 end
+
 %% Citations
 % Rosario MV, Sutton GP, Patek SN, Sawicki GS. 2016 Muscle�spring dynamics in time-limited, elastic movements.
 %   Proc. R. Soc. B 283: 20161561. http://dx.doi.org/10.1098/rspb.2016.1561
-
+% this is out of date!!!
--- lib/get_metrics.m
+++ lib/get_metrics.m
@@ -1,67 +1,69 @@
-function metrics = get_metrics(sol,transition_times,load,met_names)
-%Takes solution matrix (columns time, position, velocity)and the effective 
-%    mass and returns the metrics specified in met_names
-    % should m be load.mass+spring.mass?
-    m = load.mass;
-    EMA = load.EMA([0 0]);
+function metrics = get_metrics(sol, transition_times, load, met_names)
+    % inputs:
+    %   solution matrix with columns [time, position, velocity]
+    %   and the effective mass (EMA)
+    % outputs: 
+    %   metrics specified in met_names
 
-    sol(:,2:3)=sol(:,2:3)./EMA;
-    %define acceleration and kinetic energy as function of time 
-    if size(sol,1) < 2
-        acceleration = 0;
-    else
-        acceleration=gradient(sol(:,3))./gradient(sol(:,1));
-    end
-    kinetic_energy=.5*EMA.^2.*m.*sol(:,3).^2;
+    m = load.mass;
+    L1 = load.L1;
+    L2 = L1 ./ load.EMA([0 0]);
+    theta_0 = load.theta_0;
+    theta_f = load.theta_f;
+    y0 = sol(1,2);
+
+    %%% convert to angular space
+    sol(:,2) = asin((y0-sol(:,2)+L1.*sin(theta_0))./L1);
+    sol(:,3) = sol(:,3)./L1;
+
+    %%% determine metrics and calculate each one
     metrics=containers.Map(met_names,zeros(length(met_names),1),'UniformValues',false);
-    if isKey(metrics,'vto')
-        metrics('vto')=sol(end,3);
+
+    if isKey(metrics, 'y_latch')
+        metrics('y_latch') = y0;
     end
-    if isKey(metrics,'vmax')
-        metrics('vmax')=max(sol(:,3));
+
+    if isKey(metrics, 't_unlatch')
+        metrics('t_unlatch') = transition_times(1);
     end
-    if isKey(metrics,'tto')
-        metrics('tto')=sol(end,1);
+    if isKey(metrics, 'theta_unlatch')
+        index = find(sol(:,1)<=transition_times(1), 1, 'last');
+        metrics('theta_unlatch') = sol(index, 2);
     end
-    if isKey(metrics,'KEmax')
-        metrics('KEmax')=max(kinetic_energy);
+    if isKey(metrics, 'omega_unlatch')
+        index = find(sol(:,1)<=transition_times(1), 1, 'last');
+        metrics('omega_unlatch') = sol(index, 3);
     end
-    if isKey(metrics,'Pmax')
-        %change in kinetic energy    
-        if size(sol,1) < 2
-            metrics('Pmax') = 0;
+    if isKey(metrics, 'v_unlatch')
+        index = find(sol(:,1)<=transition_times(1), 1, 'last');
+        metrics('v_unlatch') = sol(index, 3) .* L2;
+    end
+    if isKey(metrics, 'KE_unlatch')
+        index = find(sol(:,1)<=transition_times(1), 1, 'last');
+        metrics('KE_unlatch') = 0.5 .* m .* L1.^2 .* sol(index, 3).^2;
+    end
+
+    if isKey(metrics, 't_takeoff')
+        metrics('t_takeoff') = sol(end, 1);
+    end
+    if isKey(metrics, 'theta_takeoff')
+        metrics('theta_takeoff') = sol(end, 2);
+    end
+    if isKey(metrics, 'omega_takeoff')
+        metrics('omega_takeoff') = sol(end, 3);
+    end
+    if isKey(metrics, 'v_takeoff')
+        metrics('v_takeoff') = sol(end, 3) .* L2;
+    end
+    if isKey(metrics, 'KE_takeoff')
+        metrics('KE_takeoff') = 0.5 .* m .* L1.^2 .* sol(end, 3).^2;
+    end
+
+    if isKey(metrics, 't_close')
+        index = find(sol(:,2)<=theta_f, 1, 'first');
+        if isempty(index)
+            metrics('t_close') = sol(end, 1) + (sol(end, 2) - theta_f) / sol(end, 3);
         else
-            Power = gradient(kinetic_energy)./gradient(sol(:,1));
-            metrics('Pmax')=max(Power);
+            metrics('t_close') = sol(index, 1);
         end
     end
-    if isKey(metrics,'ymax')
-        metrics('ymax')=sol(1,2);
-    end
-    if isKey(metrics,'tL')
-        metrics('tL')=transition_times(1);
-    end
-    if isKey(metrics,'yunlatch')
-        index=find(sol(:,1)<=transition_times(1),1,"last");
-        metrics('yunlatch')= sol(index, 2);
-    end
-    if isKey(metrics,'amax')
-        metrics("amax")=max(acceleration);
-    end
-    if isKey(metrics,'minumforce')
-        metrics('minumforce')= min(sol(:,11));
-    end
-    if isKey(metrics,'unlatching_motor_work_done')
-        workDone = 0;
-        for index = 1:length(sol(:,4))
-            if (index == length(sol(:,4)))
-                break
-            else
-                forceApplied = sol(index, 11);
-                distanceTravelled = sol(index+1,4)-sol(index,4);
-                workDone = workDone + (forceApplied*distanceTravelled);
-            end
-        end
-        metrics('unlatching_motor_work_done')=workDone;
-    end
-end

00d220bb1d828e32903d9af828fdd98ffb8fcacc
electrons1014 <24guppra@gmail.com>
2026-08-20 03:20:04 -0700

    adapting run-scripts for adapted model with no duplicates

--- cli-scripts/utilities.m
+++ cli-scripts/utilities.m
@@ -42,9 +42,9 @@ end
 function [lamsa_met, mda_met] = simulate(components, mets)
 
     [lamsa_sol, lamsa_transitions] = solve_lamsa(components{4}, components{5}, components{1}, components{2}, components{3});
-    lamsa_met = get_rotating_metrics(lamsa_sol, lamsa_transitions, load, mets);
+    lamsa_met = get_metrics(lamsa_sol, lamsa_transitions, load, mets);
 
     [mda_sol, mda_transitions] = solve_direct_actuation(components{4}, components{1});
-    mda_met = get_rotating_metrics(mda_sol, mda_transitions, load, mets);
+    mda_met = get_metrics(mda_sol, mda_transitions, load, mets);
 
 end
